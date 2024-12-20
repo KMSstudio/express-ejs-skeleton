d1557dcb1de9a0d11b76d924eb65a9c47ebb88f7
MS Kang <tomskang@naver.com>
2024-12-20 18:13:35 +0900

    add dependency and set .env

--- bin/www.js
+++ bin/www.js
@@ -1,13 +1,7 @@
-const fs = require('fs');
-const https = require('https');
+"use strict";
 
 const app = require("../app");
 
-const options = {
-    key: fs.readFileSync('./src/keys/key.pem'),
-    cert: fs.readFileSync('./src/keys/cert.pem')
-};
+const PORT = process.env.PORT || 3000;
 
-https.createServer(options, app).listen(443, () => {
-  console.log('HTTPS 서버가 443 포트에서 실행 중입니다.');
-});
+app.listen(PORT, () => {});
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,6 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-// router.get("/", ctrl.output.root);
-// router.post("/submit", ctrl.process.submit);
-
 router.get('/', ctrl.output.root);
-router.get('/payment', ctrl.output.payment)
-
-router.get('/return', ctrl.output.return);
-router.post('/return', ctrl.process.return);
-
-router.get('/approve', ctrl.output.approve);
-router.post("/approve", ctrl.process.approve);
 
 module.exports = router;
